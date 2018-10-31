1a6f368885e3211bbe69d8b9314442c41968398b
niu <niu@example.com>
2018-10-31 17:51:51 +0800

    11

--- pages/activity/detail/detail.js
+++ pages/activity/detail/detail.js
@@ -1,4 +1,5 @@
 // pages/activity/detail/detail.js
+const ss = require('../../../utils/time.js');
 Page({
 
   /**
@@ -12,7 +13,29 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var e = this
+    console.log(options.mid);
+    wx.request({
+      url: 'http://mall.zdcom.net.cn/api/weixin/mall.php',
+      method: 'GET',
+      data: {
+        flag: 'wx',
+        mid: options.mid,
+        type_a: 'show', 
+        itemid: options.itemid
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data);
+        e.setData({
+          'res'     : res.data,
+          'formdate': ss.formatTime(res.data.fromtime, 'Y/M/D'),
+          'totime'  :   ss.formatTime(res.data.totime, 'Y/M/D'),
+        })
+      }
+    }) 
   },
 
   /**
