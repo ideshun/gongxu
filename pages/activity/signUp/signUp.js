c77ee6d98614337f06a76e228eb6f923ee8310ab
Deshun <admin@w3h5.com>
2018-11-01 16:02:59 +0800

    update

--- pages/activity/signUp/signUp.js
+++ pages/activity/signUp/signUp.js
@@ -1,34 +1,12 @@
 // pages/activity/signUp/signUp.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     showTopTips: false,
-
-    radioItems: [
-      { name: 'cell standard', value: '0' },
-      { name: 'cell standard', value: '1', checked: true }
-    ],
-    checkboxItems: [
-      { name: 'standard is dealt for u.', value: '0', checked: true },
-      { name: 'standard is dealicient for u.', value: '1' }
-    ],
-
     date: "2016-09-01",
     time: "12:01",
-
-    countryCodes: ["+86", "+80", "+84", "+87"],
-    countryCodeIndex: 0,
-
-    countries: ["中国", "美国", "英国"],
-    countryIndex: 0,
-
-    accounts: ["微信号", "QQ", "Email"],
-    accountIndex: 0,
-
-    isAgree: false
   },
   showTopTips: function () {
     var that = this;
@@ -41,79 +19,43 @@ Page({
       });
     }, 3000);
   },
-  radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value);
-
-    var radioItems = this.data.radioItems;
-    for (var i = 0, len = radioItems.length; i < len; ++i) {
-      radioItems[i].checked = radioItems[i].value == e.detail.value;
-    }
-
-    this.setData({
-      radioItems: radioItems
-    });
-  },
-  checkboxChange: function (e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
-
-    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
-    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-      checkboxItems[i].checked = false;
-
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (checkboxItems[i].value == values[j]) {
-          checkboxItems[i].checked = true;
-          break;
+  subEct: function () {
+    let _this = this
+    var values;
+    values = e.detail.value;
+    console.log(values);
+    _this.setData({ disabled: true });
+    wx.request({
+      url: 'http://mall.zdcom.net.cn/api/weixin/mall.php',
+      data: {
+        'flag': 'wx',
+        'type_a': 'add_exhibit',
+        'truename': values.detial,
+        // 'postcode' : e.detail.value.postcode,
+        'mobile': values.phone,
+        'addr': values.detail,
+        'username': 'admin'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res)
+        if (res.data.error != '') {
+          wx.showToast({
+            title: '添加成功',
+            icon: 'success',
+            duration: 2000
+          })
         }
       }
-    }
-
-    this.setData({
-      checkboxItems: checkboxItems
-    });
-  },
-  bindDateChange: function (e) {
-    this.setData({
-      date: e.detail.value
     })
   },
-  bindTimeChange: function (e) {
-    this.setData({
-      time: e.detail.value
-    })
-  },
-  bindCountryCodeChange: function (e) {
-    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      countryCodeIndex: e.detail.value
-    })
-  },
-  bindCountryChange: function (e) {
-    console.log('picker country 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      countryIndex: e.detail.value
-    })
-  },
-  bindAccountChange: function (e) {
-    console.log('picker account 发生选择改变，携带值为', e.detail.value);
-
-    this.setData({
-      accountIndex: e.detail.value
-    })
-  },
-  bindAgreeChange: function (e) {
-    this.setData({
-      isAgree: !!e.detail.value.length
-    });
-  },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -7,21 +7,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-    byname: 'app.globalData.Nickname',
-    nickName: app.globalData.Nickname,
+    byname: '',
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     obtain: '获得了0次赞，发布了0条口碑',
-    avatarUrl: app.globalData.AvatarUrl,
+    avatarUrl: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var e = this;
-    e.setData({
-      byname: app.globalData.Nickname,
+    var that = this;
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              that.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+              })
+              console.log(res.userInfo)
+            }
+          })
+        }
+      }
     })
-    console.log(app.globalData.Nickname);
   },
 
   /**
