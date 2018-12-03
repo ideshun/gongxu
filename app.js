7808863092f07010e5196efb9a709b3bada4f491
gaopeng168 <3279275616@qq.com>
2018-12-03 14:19:19 +0800

    11111

--- app.js
+++ app.js
@@ -8,21 +8,26 @@ App({
     Province: ''
   },
   onLaunch: function () {
-    var e = this
+    //let that = this;
+    //that.checkLoginStatus();
+    //this.login();
+    var e = this;
     wx.login({
       success: function (res) {
-        // console.log(res.code)
+        //console.log(res);
         if (res.code) {
+          //console.log(9999),
           wx.getUserInfo({ //得到rawData, signatrue, encryptData
             success: function (data) {
-            //  console.log(data);
+              //console.log(data);
               var rawData = data.rawData;
               var signature = data.signature;
               var encryptedData = data.encryptedData;
               var iv = data.iv;
-
               //发起网络请求
               wx.request({
+                url: 'https://mall.zdcom.net.cn/api/wxapp/login.php',
+                method: 'GET',
                 data: {
                   'flag': 'wx',
                   'code': res.code,
@@ -35,15 +40,36 @@ App({
                   'content-type': 'application/json' // 默认值
                 },
                 success: function (res1) {
+                 // console.log(6666);
                   //console.log(res1);
+                  e.globalData.Nickname = res1.data.nickName,
+                    e.globalData.AvatarUrl = res1.data.avatarUrl,
+                    e.globalData.Province = res1.data.province,
+                    e.globalData.City = res1.data.city,
+                    e.globalData.Openid = res1.data.openId,
+                    e.globalData.Country = res1.data.country,
+                    e.globalData.Gender = res1.data.gender,
+                    e.globalData.Language = res1.data.language,
+                    e.globalData.Timestamp = res1.data.watermark.timestamp
+                  //console.log(e);
+                  wx.setStorageSync('openid', e.globalData.Openid);
                 }
               })
+
             }
           })
         } else {
           console.log('登录失败！' + res.errMsg)
         }
       }
+    });
+  },
+  showInfo: function (info = 'error', icon = 'none') {
+    wx.showToast({
+      title: info,
+      icon: icon,
+      duration: 1500,
+      mask: true
     });
   }
 
