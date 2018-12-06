374378840fd49359d7c658109920162904f59c0b
gaopeng168 <3279275616@qq.com>
2018-12-06 18:06:31 +0800

    11

--- pages/personal/A-myFavour/A-myFavour.js
+++ pages/personal/A-myFavour/A-myFavour.js
@@ -1,4 +1,5 @@
 // pages/personal/A-myFavour/A-myFavour.js
+var common = require('../../../utils/common.js');
 Page({
 
   /**
@@ -12,7 +13,25 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var e = this;
+      wx.request({
+        url: common.url_mall, //仅为示例，并非真实的接口地址
+        method: 'GET',
+        data: {
+          flag: 'wx',
+          type_a: 'fav_list',
+          openid: wx.getStorageSync('openid'),
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: function (res) {
+          console.log(res)
+          e.setData({
+            favList:res.data
+          })
+        }
+      })
   },
 
   /**
