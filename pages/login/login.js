d81c609eba0a09f196e86da3f94f32c651fca5aa
Deshun <admin@w3h5.com>
2018-12-06 17:33:28 +0800

    update

--- pages/login/login.js
+++ pages/login/login.js
@@ -6,22 +6,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    avatarUrl: "https://mall.zdcom.net.cn/file/upload/images/boy.png",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var is_login = wx.getStorageSync('is_login');
-    if (is_login == 1){
+    if (is_login == 1) {
       wx.switchTab({
         url: '/pages/index/index'
       })
     }
   },
   //授权申请
-  bindGetUserInfo: function (e) {
+  bindGetUserInfo: function(e) {
     if (e.detail.userInfo) {
       // console.log(app);
       //用户按了允许授权按钮
@@ -30,22 +31,23 @@ Page({
       wx.request({
         url: 'https://mall.zdcom.net.cn/api/weixin/login.php',
         data: {
-          'wx_login':1,
+          'wx_login': 1,
           openid: app.globalData.Openid,
           nickName: app.globalData.Nickname,
           avatarUrl: app.globalData.AvatarUrl,
           province: app.globalData.province,
-          city: app.globalData.city
+          city: app.globalData.city,
         },
         header: {
           'content-type': 'application/json'
         },
-        success: function (res) {
+        success: function(res) {
           //从数据库获取用户信息
           // that.queryUsreInfo();
           // console.log(res);
           // console.log(wx.getStorageSync('is_login'));
           console.log("插入小程序登录用户信息成功！");
+
         }
       });
       //授权成功后，跳转进入小程序首页
@@ -59,7 +61,7 @@ Page({
         content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
         showCancel: false,
         confirmText: '返回授权',
-        success: function (res) {
+        success: function(res) {
           if (res.confirm) {
             console.log('用户点击了“返回授权”')
           }
@@ -71,49 +73,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
